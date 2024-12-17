39dc56279ac10054c6b8c5abbdf2a910336a6d97
Ying <563520646@qq.com>
2024-12-17 15:51:04 -0500

    update README

--- README.md
+++ README.md
@@ -1,1 +1,122 @@
 # LLM-for-Study-Plan
+
+* **<u>Framwork</u>**：Torch
+
+* **<u>Library</u>**
+
+  * Unsloth
+  * Huggingface-Transformers
+  * SFTTrainer
+
+* **<u>ComputationResources</u>**
+
+  * HPC；Autodl
+
+* **<u>Components</u>**
+
+  * DatasetPreprocessing => DataSetDownload
+    * 3 Task
+  * Model => Freezing Most, Reset some 
+    * ModelChoice：Which model? Which layer's weights to be trained?
+    * FinetuningTricks：Only hyperpara or add Layers?
+    * Loss：What loss？
+    * TrainingWay：Multi-object/Multi-task
+  * Metrics => Precison, Recall
+
+* **<u>Technical</u>** 
+
+  * Joint Learning?
+
+* **<u>Difficulty</u>**
+
+  * computation resources
+  * training speed
+
+* **<u>Application</u>**
+
+  * For presentation： different scenario => based on chatbot
+
+    * P0
+      * solve math problems
+      * generate a study plan (grade, weakeness domain)
+    * P1
+      * input error set (answer question + analyze weakness + generate plan)
+
+  * For report
+
+    * compare with baseline
+
+      * Math QA => public dataset
+
+      * Concept Extracting => self-customed dataset + model-labeld dataset (train/test)
+
+        > How many data are required
+
+* **<u>STEPS|MileStones</u>**
+
+  * **Model Construction**
+
+    * Find public github repo + adapt to our tasks(data processing)
+
+      > REF: [Github](https://github.com/togethercomputer/finetuning)
+      >
+      > [GPT-4 Finetuned](https://github.com/liutiedong/goat)
+      >
+      > Math-Finetuning Github with metrics
+      >
+      > Finetuning Lllama3 8B Github
+
+    * Llam3 3B/8B + Lora
+
+      > 8B: 14GB, 16bit  
+      >
+      > 3B: 4bit
+      > [Tutorial](https://github.com/amitlevy/finetune_llama_3_own_data/blob/master/llama3_8b_finetune_own_data.ipynb)
+
+    * Finetune on Math dataset
+
+    * Hyperpara: lr
+
+    * Resources
+
+      > VastAi, 1 4090 =>  1 24GB, 0.35
+
+    * **Checkpoint**
+
+      * **Accuaray Metrics for this two, accuracy/solveRate**
+
+        > Rule-base extraction
+        >
+        > ChatGpt extraction
+
+      * **Training Framwork**
+
+  * **PromptEngineering**
+
+    * target: find best prompt to solve question
+
+      > Metrics |  How do people measures prompt improvements
+      >
+      > refer COT, still accuracy
+      >
+      > refer ROSCOE => evaluate reasoning
+      >
+      > refer Confidence of Hallucination => study plan (confidence higher than baseline)
+
+    * only prompt
+
+    * prompt + sft
+
+    * **Checkpoint**
+
+      * Inference Framwork
+      * Reasoning Metrics
+
+  * **PresentationWay**
+
+    * Quantize + Deploy on phone
+    * Comparison: Quantized Version
+
+  * **Serving**
+
+    * Ollama 
--- src/test.py
+++ src/test.py
@@ -7,69 +7,52 @@ from torch.utils.data import DataLoader
 from utils.formatter import *
 from models.generator import model_generator
 
-baselines = [
-    "unsloth/Meta-Llama-3.1-8B",
-    "unsloth/Llama-3.2-3B-Instruct",
-    "unsloth/Llama-3.2-1B-Instruct",
-]
 
-# load data
-ds = load_dataset("openai/gsm8k", "main")
-samples = ds["train"]
-test_dataset = ds["test"]
 
-test_data = test_dataset.take(16)
-"""
-test_data = test_dataset.take(8)
-test_data.set_format(type="torch")
-test_dataloader = DataLoader(test_data, batch_size=2)
-for batch in test_dataloader:
-    messages = nshot_chats_batch(batch, samples, 2)
-    response = get_response(model, tokenizer, messages)
-    pred_answers = [extract_ans_from_response(answer) for answer in response]
-    true_answers = [extract_ans_from_response(answer) for answer in batch['answer']]
-    for i in range(len(pred_answers)):
-        total += 1
-        if pred_answers[i] == true_answers[i]:
-            correct += 1
-"""
+def main():
+    baselines = [
+        "./lora_model",
+        # "unsloth/Meta-Llama-3.1-8B",
+        "unsloth/Llama-3.2-3B-Instruct",
+        "unsloth/Llama-3.2-1B-Instruct",
+    ]
 
-# test accuracy for different models
-acc_data = []
+    # test accuracy for different models
+    acc_data = []
 
-for model_name in baselines:
-    print(f"Testing {model_name}")
-    start_time = time.perf_counter()
-    if "lora_model" in model_name:
-        model, tokenizer = model_generator(model_name, 2048, True)
-    else:
-        model, tokenizer = model_generator(model_name)
+    for model_name in baselines:
+        print(f"Testing {model_name}")
+        start_time = time.perf_counter()
+        if "lora_model" in model_name:
+            model, tokenizer = model_generator(model_name, 2048, True)
+        else:
+            model, tokenizer = model_generator(model_name)
 
-    FastLanguageModel.for_inference(model)  # Enable native 2x faster inference
+        FastLanguageModel.for_inference(model)  # Enable native 2x faster inference
 
-    total = correct = 0
-    for item in tqdm(test_data):
-        messages = nshot_chats(nshot_data=samples, n=8, question=item["question"])
-        response = get_response(model, tokenizer, messages)
-        print("============== QUESTION ==============")
-        print(item["question"])
-        print("============== ANSWER ==============")
-        print(item["answer"])
-        print("============== RESPONSE ==============")
-        print(response)
+        total = correct = 0
+        for item in tqdm(test_data):
+            messages = nshot_chats(nshot_data=samples, n=2, question=item["question"])
+            response = get_response(model, tokenizer, messages)
+            # print("============== QUESTION ==============")
+            # print(item["question"])
+            # print("============== ANSWER ==============")
+            # print(item["answer"])
+            # print("============== RESPONSE ==============")
+            # print(response)
 
-        pred_ans = extract_ans_from_response(response)
-        true_ans = extract_ans_from_response(item["answer"])
+            pred_ans = extract_ans_from_response(response)
+            true_ans = extract_ans_from_response(item["answer"])
 
-        print("============== EXTRACT_ANS ==============")
-        print(f"pred_ans: {pred_ans}")
-        print(f"true_ans: {true_ans}")
+            # print("============== EXTRACT_ANS ==============")
+            # print(f"pred_ans: {pred_ans}")
+            # print(f"true_ans: {true_ans}")
 
-        total += 1
-        if pred_ans == true_ans:
-            correct += 1
+            total += 1
+            if pred_ans == true_ans:
+                correct += 1
 
-    accuracy = f"{correct/total:.3f}"
-    acc_data.append([model_name, accuracy])
-    print(f"Total Accuracy: {accuracy}")
-    print(f"Duration: {time.perf_counter() - start_time}")
+        accuracy = f"{correct/total:.3f}"
+        acc_data.append([model_name, accuracy])
+        print(f"Total Accuracy: {accuracy}")
+        print(f"Duration: {time.perf_counter() - start_time}")
